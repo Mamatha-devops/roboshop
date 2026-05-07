69054cc07bd8f7d729c49aedf62de882f98b27a7
Mamatha-devops <vangeti.mamata@gmail.com>
2026-05-07 13:33:37 -0400

    configuring fronend

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,16 @@
 #!bin/bash
 
+ID=$(id -u)
+if [ $ID -ne 0 ]; then
+  echo "You should run this script as root  user or with sudo privileges"
+  exit 1
+fi
+
 echo "disabling  default nginx version " 
 dnf module diable nginx -y 
+
 echo "enabling nginx 1.24 version"
 dnf module enable nginx:1.24 -y
+
 echo "installing nginx"
 dnf install nginx -y 
